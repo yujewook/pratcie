a5a4554fe73baf73142b3777eeae3c2b5eabd2a5
Ryu <107185709+yujewook@users.noreply.github.com>
2024-02-14 22:13:28 +0900

    Add files via upload

--- pratice/src/main/java/com/pratice/serviceImple/RegesiterServiceImpl.java
+++ pratice/src/main/java/com/pratice/serviceImple/RegesiterServiceImpl.java
@@ -1,9 +1,13 @@
 package com.pratice.serviceImple;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.protobuf.TextFormat.ParseException;
 import com.pratice.dao.MemberDao;
 import com.pratice.dto.MemerInfoDto;
 import com.pratice.dto.SaveResultDto;
@@ -23,9 +27,9 @@ public class RegesiterServiceImpl implements RegesiterService {
 //validation
 //////////////////////////////////////////////////////////////////////////////
 		if ( inDto.getId() == null || inDto.getId() == "") {
-	    //outDto.setRestltMsg("아이디 미입력");
-		msg = "아이디 미입력";
-		throw new RuntimeException(msg);
+		    //outDto.setRestltMsg("아이디 미입력");
+			msg = "아이디 미입력";
+			throw new RuntimeException(msg);
 		}
 
 		if ( inDto.getPwd() == null || inDto.getPwd() == "") {
@@ -33,6 +37,30 @@ public class RegesiterServiceImpl implements RegesiterService {
 			msg = "패스워드 미입력";
 			throw new RuntimeException(msg);
 		}
+		
+		if ( inDto.getBirth() == null || inDto.getBirth() == "") {
+			outDto.setRestltMsg("생일 미입력");
+			msg = "생일 미입력";
+			throw new RuntimeException(msg);
+		} else {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+			try {
+			    Date date = sdf.parse(inDto.getBirth());
+			    Date currentDate = new Date();
+			    if (!date.equals(currentDate)) {
+				    msg = "올바른 날짜 형식이 아닙니다.";
+				    throw new RuntimeException(msg);
+			    }
+			} catch (java.text.ParseException e) {
+			    e.printStackTrace();
+			    msg = "올바른 날짜 형식이 아닙니다.";
+			    throw new RuntimeException(msg);
+			} catch (Exception e) {
+			    e.printStackTrace();
+			}
+		}
+		
+		
 //////////////////////////////////////////////////////////////////////////////
 //logicStart
 //////////////////////////////////////////////////////////////////////////////
